055fba0ea3f690dfdcdab751b30b24d544f8031d
withnic <mybest.is.next.one+github@gmail.com>
2019-09-23 23:48:07 +0900

    :+1: add graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -40,14 +45,36 @@ func main() {
 		}); err != nil {
 			logger.Sugar().Errorw("failed to render json",
 				"url", request.URL,
-				"error", err,
+				"error", err.Error(),
 			)
 		}
 	})
 
-	if err := http.ListenAndServe(":8080", router); err != nil {
-		logger.Sugar().Errorw("failed to serve",
-			"err", err,
-		)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+
+	go func() {
+		logger.Sugar().Infow("Listening",
+			"addr", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil {
+			logger.Sugar().Errorw("failed to serve",
+				"error", err.Error(),
+			)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	// https://docs.docker.com/engine/reference/commandline/stop/
+	signal.Notify(quit, syscall.SIGTERM, os.Interrupt)
+	logger.Sugar().Infof("SIGNAL %d received, then shutting down...", <-quit)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		// Error from closing listeners, or context timeout:
+		logger.Sugar().Errorw("Failed to gracefully shutdown",
+			"error", err.Error())
+	}
+	logger.Sugar().Info("Server shutdown")
 }
